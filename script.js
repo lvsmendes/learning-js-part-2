e8f1243958e85e1261657a53bdb8f4a4dabd0293
lvsmendes <lvsmendes2@gmail.com>
2025-08-23 11:38:17 -0300

    parte dois do aprendizado js comecando com functions

---
+++ script.js
@@ -0,0 +1,42 @@
+'use strict'; 
+/*
+let hasDriversLicense = false;
+const passTest = true;
+
+if (passTest) hasDriversLicense = true;
+if (hasDriversLicense) console.log('i can drive');
+
+//const interface = 'Áudio';
+//const private = 534;
+
+*/
+
+function logger() {
+    console.log('My name is lucas');
+}
+
+//calling / running / invoking function
+logger();
+logger();
+logger();
+
+function fruitProcessor(apples, oranges){
+    console.log(apples, oranges);
+    const juice = `juice with ${apples} apples and ${oranges} oranges.`;
+    return juice;
+}
+
+const appleJuice = fruitProcessor(5, 0);
+console.log(appleJuice);
+
+const appleOrangeJuice = fruitProcessor(2, 4);
+console.log(appleOrangeJuice);
+
+
+function meetProcessor(beef, pork, chicken){
+    const meet = `minced meat with ${beef} beef, ${pork} pork, and ${chicken} chicken.`;
+    return meet;
+}
+
+const beefPorkChickenMeet = meetProcessor(1, 1, 1);
+console.log(beefPorkChickenMeet);
